987b0330c7d5c0a93e649b9df65a24c6679b21d8
Varma Kaushal <varmakaushal33@gmail.com>
2025-05-27 16:11:57 +0530

    added new Todo list pr

---
+++ Todo-react/src/components/Todo.jsx
@@ -0,0 +1,16 @@
+
+const Todo = () => {
+    return (
+        <div className="min-h-screen bg-gradient-to-br from-gray-900 via-purple-900 to-gray-900 text-white">
+            <h1 className="text-5xl bg-gradient-to-r from-purple-400 via-pink-400 to-blue-400 bg-clip-text text-transparent text-center capitalize py-6 font-extrabold">
+                todo list
+            </h1>
+            <p className="text-xl text-gray-300 mb-8 text-center font-mono">
+                Mint your productivity • Collect your achievements
+            </p>
+            
+        </div>
+    )
+}
+
+export default Todo
